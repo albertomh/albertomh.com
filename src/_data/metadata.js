c0afcb63067715909f40e96e7bebca98bbd69489
Alberto Morón Hernández <alberto.moronh@gmail.com>
2022-05-21 10:36:26 +0100

    Show date range in footer

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -1,10 +1,17 @@
 module.exports = function() {
     const today8601 = new Date().toISOString().slice(0, 10);
 
+    let footerDate = "2021";
+    const curYear = parseInt(today8601.slice(0, 4));
+    if (curYear > parseInt(footerDate)) {
+        footerDate = `${footerDate}-${curYear}`;
+    }
+
     return {
         title: "Alberto MH",
         blurb: "Alberto is a software engineer based in Bristol, UK.",
         createdAt: today8601,
-        updatedAt: today8601
+        updatedAt: today8601,
+        footerDate
     };
 };
