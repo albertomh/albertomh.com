582b62e0c930eddca92704ac992834ee9a6af915
Alberto Morón Hernández <alberto.moronh@gmail.com>
2025-05-16 18:24:10 +0100

    fix: stop hardcoding last modified date for 'about' page

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -7,11 +7,18 @@ module.exports = function() {
         footerDate = `${footerDate}-${curYear}`;
     }
 
+    const todayISO = new Date().toISOString();
+    const todayMonthYear = (
+        new Date(todayISO)
+        .toLocaleString('en-GB', { month: 'long', year: 'numeric' })
+    );
+
     return {
         title: "Alberto MH",
-        blurb: "Alberto is a software engineer based in Bristol, UK.",
+        blurb: "Alberto is a software engineer based in Somerset, UK.",
         createdAt: today8601,
         updatedAt: today8601,
+        todayMonthYear,
         footerDate
     };
 };
