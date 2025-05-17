147a6268c1443a1adfe4253077141d766f6f2f70
Alberto Morón Hernández <albertomh@users.noreply.github.com>
2025-05-17 10:36:08 +0100

    chore: add pre-commit hooks (#4)
    * chore: add pre-commit hooks
    
    * chore: apply fixes from pre-commit hooks
    
    * chore: ignore previous commit since only had formatting changes

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -1,17 +1,17 @@
-module.exports = function() {
+module.exports = () => {
     const today8601 = new Date().toISOString().slice(0, 10);
 
     let footerDate = "2021";
-    const curYear = parseInt(today8601.slice(0, 4));
-    if (curYear > parseInt(footerDate)) {
+    const curYear = Number.parseInt(today8601.slice(0, 4));
+    if (curYear > Number.parseInt(footerDate)) {
         footerDate = `${footerDate}-${curYear}`;
     }
 
     const todayISO = new Date().toISOString();
-    const todayMonthYear = (
-        new Date(todayISO)
-        .toLocaleString('en-GB', { month: 'long', year: 'numeric' })
-    );
+    const todayMonthYear = new Date(todayISO).toLocaleString("en-GB", {
+        month: "long",
+        year: "numeric",
+    });
 
     return {
         title: "Alberto MH",
@@ -19,6 +19,6 @@ module.exports = function() {
         createdAt: today8601,
         updatedAt: today8601,
         todayMonthYear,
-        footerDate
+        footerDate,
     };
 };
