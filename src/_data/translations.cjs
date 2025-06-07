8f33d89ceec544104ac97e71014f7bb3e2967961
Alberto Morón Hernández <alberto.moronh@gmail.com>
2025-06-07 09:21:47 +0100

    feat: add the translations data file

---
+++ src/_data/translations.cjs
@@ -0,0 +1,20 @@
+module.exports = () => {
+    return {
+        _includes: {
+            navbar: {
+                posts: {
+                    en: "Posts",
+                    es: "Artículos",
+                },
+                about: {
+                    en: "About",
+                    es: "Acerca de mí",
+                },
+                cv: {
+                    en: "My CV",
+                    es: "Mi CV",
+                },
+            },
+        },
+    };
+};
